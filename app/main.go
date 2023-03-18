88e934172c9e1e1552798e036999d250881348b5
Matsukura <heuchannsight@yahoo.co.jp>
2023-03-18 09:37:25 +0900

    2st stage

--- app/main.go
+++ app/main.go
@@ -13,8 +13,6 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage!
-	//
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 	//
